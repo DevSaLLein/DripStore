99ccb4234c70cbe3732934d2d61b7b39f9b562fc
http-sallein <sallein.caasi@gmail.com>
2025-02-10 18:23:29 -0300

    styled components added, theme created

---
+++ src/themes.ts
@@ -0,0 +1,27 @@
+export const theme = {
+
+    colors: {
+
+
+        darkGray: "#1F1F1F",
+        darkGray2: "#474747",
+        darkGray3: "#666666",
+
+        lightGray: "#8F8F8F",
+        lightGray2: "#CCCCCC",
+        lightGray3: "#F5F5F5",
+
+        white: "#FFFFFF",
+
+        primary: "#C92071",
+        secondary: "#B5B6F2",
+        tertiary: "#991956",
+
+        error: "#EE4266",
+        success: "#52CA76",
+        warning: "#F6AA1C",
+
+        primaryGradient: "linear-gradient(90deg, #C92071 0%, #EDABCA 100%)",
+        secondaryGradient: "linear-gradient(90deg, #B5B6F2 0%, #EFEFFF 100%)",
+    },
+}
