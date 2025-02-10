9d16edaccb655bafdf5a0e4d8d5e59a3b467f2e5
http-sallein <sallein.caasi@gmail.com>
2025-02-10 18:08:07 -0300

    delete old archives

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { AppRoutes } from './routes/AppRoutes';
-
-export function App() {
-    return (
-        <>
-            <AppRoutes />
-        </>
-    );
-}
--- src/pages/NotFoundPage.jsx
+++
@@ -1,7 +0,0 @@
-export function NotFoundPage() {
-    return (
-        <section>
-            <h1> Página não encontrada </h1>
-        </section>
-    );
-}
